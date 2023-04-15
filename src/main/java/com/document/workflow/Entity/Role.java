e94b5c2702599462506718b98ab438bc3964ad1d
Marzhan Izteleu <marzhanizteleuova@gmail.com>
2023-04-16 00:16:06 +0600

    add security

---
+++ src/main/java/com/document/workflow/Entity/Role.java
@@ -0,0 +1,19 @@
+package com.document.workflow.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ROLES")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name")
+    private ERole name;
+
+
+}
